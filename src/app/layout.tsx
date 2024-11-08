780e6a20a6f5d28b3c8afb20fb6ea09187995b9a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-08 23:07:19 +0000

    Update copy and styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './styles.tailwind.css'
 import MenuBar from './components/menuBar'
 
 export const metadata: Metadata = {
-  title: 'Dan Edwards Analytics',
+  title: `Dan's Analytics`,
 }
 
 export const viewport: Viewport = {
@@ -23,10 +23,8 @@ export default function RootLayout({
       className="h-screen overflow-hidden bg-gradient-to-tr from-sky-50 via-indigo-100 to-cyan-100"
     >
       <body className="h-screen overflow-hidden">
-        <div className="w-full max-w-[94rem] mx-auto flex h-screen flex-col lg:flex-row gap-6 py-6 px-4 sm:px-8 max-sm:pb-24">
-          <div className="lg:w-72">
-            <MenuBar />
-          </div>
+        <div className="w-full max-w-[94rem] mx-auto flex h-screen flex-col md:flex-row gap-6 py-6 px-4 sm:px-8 max-sm:pb-24">
+          <MenuBar />
           <div className="w-full flex-1 bg-white bg-opacity-75 border border-blue-500/30 rounded-lg py-8 px-6">
             <h1>{children}</h1>
           </div>
