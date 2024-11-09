3345dc4b7fc847ee5003eefd7cdd2681394f4f36
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-09 12:06:17 +0000

    Hide MenuBar if not signed in

--- src/app/components/menuBar/index.tsx
+++ src/app/components/menuBar/index.tsx
@@ -2,6 +2,7 @@
 import DesktopMenu from './DesktopMenu'
 import { sitesData } from '@/library/sites'
 import MobileMenu from './MobileMenu'
+import { useApp } from '../AppProvider'
 
 export interface MenuItemInterface {
   displayName: string
@@ -11,6 +12,11 @@ export interface MenuItemInterface {
 const menuItems: MenuItemInterface[] = [{ displayName: 'Home', slug: '/' }]
 
 export default function MenuBar() {
+  const { signedIn } = useApp()
+
+  if (!signedIn) {
+    return null
+  }
   return (
     <>
       <div className="hidden md:block lg:w-72">
