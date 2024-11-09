30eb6260dc51cd791733af58abbabab2e1104163
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-09 09:54:20 +0000

    Extract subcomponents

--- src/app/components/menuBar/DesktopMenu.tsx
+++ src/app/components/menuBar/DesktopMenu.tsx
@@ -1,74 +1,18 @@
 'use client'
 import { useState } from 'react'
-import MenuItem from './MenuItem'
 import { usePathname } from 'next/navigation'
 
 import { MenuItemInterface } from '.'
 import { Site } from '@/library/sites'
 import Divider from '../Divider'
-
-type TimeRange = '30days' | 'alltime'
-
-function NavigationLinks({
-  menuItems,
-  currentPath,
-}: {
-  menuItems: MenuItemInterface[]
-  currentPath: string
-}) {
-  return menuItems.map((menuItem) => (
-    <MenuItem
-      key={menuItem.slug}
-      href={menuItem.slug}
-      displayName={menuItem.displayName}
-      isActive={currentPath === '/'}
-    />
-  ))
-}
-
-function SiteLinks({
-  sites,
-  currentPath,
-}: {
-  sites: Site[]
-  currentPath: string
-}) {
-  return sites.map((site) => (
-    <MenuItem
-      key={site.slug}
-      href={site.slug}
-      displayName={site.displayName}
-      isActive={currentPath === `/${site.slug}`}
-    />
-  ))
-}
-
-interface TimeFiltersProps {
-  selectedRange: TimeRange
-  onRangeChange: () => void
-}
-
-function TimeFilters({ selectedRange, onRangeChange }: TimeFiltersProps) {
-  return (
-    <div className="flex flex-col gap-4 w-fit">
-      <MenuItem
-        displayName="Last 30 days"
-        isActive={selectedRange === '30days'}
-        onClick={() => onRangeChange('30days')}
-      />
-      <MenuItem
-        displayName="All time"
-        isActive={selectedRange === 'alltime'}
-        onClick={() => onRangeChange('alltime')}
-      />
-    </div>
-  )
-}
+import TimeFilters, { TimeRange } from './TimeFilters'
+import { NavigationLinks, SiteLinks } from './MenuLinks'
 
 interface DesktopMenuProps {
   menuItems: MenuItemInterface[]
   sites: Site[]
   initialTimeRange?: TimeRange
+  // eslint-disable-next-line no-unused-vars
   onTimeRangeChange?: (range: TimeRange) => void
 }
 
@@ -87,7 +31,7 @@ export default function DesktopMenu({
   }
 
   return (
-    <div className="lg:w-72">
+    <>
       <nav className="space-y-4">
         <NavigationLinks menuItems={menuItems} currentPath={path} />
         <Divider />
@@ -98,6 +42,6 @@ export default function DesktopMenu({
         selectedRange={timeRange}
         onRangeChange={handleTimeRangeChange}
       />
-    </div>
+    </>
   )
 }
