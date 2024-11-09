3f7819166852fc9c69bb999145bea491bcf3185f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-09 12:07:16 +0000

    Remove TwoColumnLayout

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,49 +1,56 @@
-'use client';
-import TwoColumnLayout from '../components/TwoColumnLayout';
-import { Form, Input, PasswordInput, FormSpacer } from './components/Form';
-import { Button } from './components/form/SubmitButton';
+'use client'
+import { Input, PasswordInput } from './components/Form'
+import { Button } from './components/form/SubmitButton'
+import { useApp } from '../components/AppProvider'
+import FeedbackMessage from './components/FeedbackMessage'
 
 function handleSubmit() {
-	return;
+  return
 }
 
 export default function SignInPage() {
-	return (
-		<TwoColumnLayout
-			title={`Sign in`}
-			intro={`Please sign in to use this site`}
-			columnOne={
-				// <FeedbackMessage />
-				<Form onSubmit={handleSubmit}>
-					<FormSpacer />
-					<Input
-						autoComplete="email"
-						dataTestID="email-input"
-						id="email"
-						label="Email"
-						name="email"
-						type="email"
-						value={''}
-						onChange={() => null}
-					/>
-					<PasswordInput
-						autoComplete="current-password"
-						dataTestID="password-input"
-						id="password"
-						label="Password"
-						name="password"
-						value={''}
-						onChange={() => null}
-					/>
-					<Button
-						dataTestID="sign-in-button"
-						disabled={false}
-						text={'Sign in'}
-						type="submit"
-						variant={'primary'}
-					/>
-				</Form>
-			}
-		/>
-	);
+  const { updateAppState, signedIn } = useApp()
+
+  function handleToggle(event: React.MouseEvent) {
+    event.preventDefault()
+    updateAppState({
+      signedIn: !signedIn,
+      message: !signedIn ? null : 'Please sign in',
+    })
+  }
+
+  return (
+    <div className="h-full w-full flex items-center justify-center overflow-hidden">
+      <form className="max-w-md w-full space-y-4" onSubmit={handleSubmit}>
+        <Input
+          autoComplete="email"
+          dataTestID="email-input"
+          id="email"
+          label="Email"
+          name="email"
+          type="email"
+          value={''}
+          onChange={() => null}
+        />
+        <PasswordInput
+          autoComplete="current-password"
+          dataTestID="password-input"
+          id="password"
+          label="Password"
+          name="password"
+          value={''}
+          onChange={() => null}
+        />
+        <FeedbackMessage message="Incorrect credentials" />
+        <Button
+          dataTestID="sign-in-button"
+          disabled={false}
+          text={signedIn ? 'Sign Out' : 'Sign In'}
+          type="submit"
+          variant={'primary'}
+          onClick={handleToggle}
+        />
+      </form>
+    </div>
+  )
 }
