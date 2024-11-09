48714a5855b5d82334ba0bba96490db14777796e
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-09 09:52:26 +0000

    Add PlaceholderContent

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { notFound } from 'next/navigation'
 import { sitesData } from '@/library/sites'
 import PageHeader from '../components/PageHeader'
 import Divider from '../components/Divider'
+import PlacehoderContent from '../components/PlaceholderContent'
 
 interface Props {
   params: Promise<{
@@ -24,6 +25,7 @@ export default function SitePage({ params }: Props) {
     <>
       <PageHeader title={site.displayName} intro={site.description} />
       <Divider margin="my-6" />
+      <PlacehoderContent />
     </>
   )
 }
---
+++ src/app/components/PlaceholderContent.tsx
@@ -0,0 +1,8 @@
+export default function PlacehoderContent() {
+  return [1, 2, 3, 4, 5, 6].map((item) => (
+    <p
+      key={item}
+      className="text-slate-600 my-6"
+    >{`Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt omnis optio vel officiis tempora, ducimus repellat porro quibusdam molestias velit, sunt praesentium! Veritatis repellat inventore deserunt quas animi labore officia.`}</p>
+  ))
+}
