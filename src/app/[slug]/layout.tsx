456f06943e27831823e8cebf657f0b4b62722b0c
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-10 21:02:38 +0000

    Create layout.tsx

---
+++ src/app/[slug]/layout.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx'
+import { ReactNode } from 'react'
+
+export default function SiteAnalyticsLayout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+    <div
+      role="main"
+      className={clsx(
+        'w-full flex-1 bg-white bg-opacity-75',
+        'md:border border-blue-500/30 md:rounded-lg',
+        'pt-8 px-6 ',
+        'md:pt-12 md:px-12 pb-60',
+        'overflow-y-auto'
+      )}
+    >
+      <div data-header-offset className="md:hidden h-[70px]" />
+      {children}
+    </div>
+  )
+}
