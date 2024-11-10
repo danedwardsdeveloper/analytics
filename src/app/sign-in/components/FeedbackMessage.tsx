c4bb378b078a1ba53ac118b672d5263b0167c636
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-10 21:00:17 +0000

    Rename components

--- src/app/sign-in/components/FeedbackMessage.tsx
+++
@@ -1,7 +0,0 @@
-export default function FeedbackMessage({ message }: { message: string }) {
-  return (
-    <div className="w-full text-center h-6 py-6">
-      <span className="font-medium text-red-500">{message}</span>
-    </div>
-  )
-}
