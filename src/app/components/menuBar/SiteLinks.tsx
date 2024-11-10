c2d3e90566d672693f8ce04726dfaaf428850743
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-10 11:36:19 +0000

    Create SiteLinks.tsx

---
+++ src/app/components/menuBar/SiteLinks.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx'
+import Link from 'next/link'
+import { sitesData } from '@/library/sites'
+import { usePathname } from 'next/navigation'
+import { baseStyles, colourStyles, sizeStyles } from './styles'
+
+export default function SiteLinks({ gap = 'gap-y-1' }: { gap?: string }) {
+  const currentPath = usePathname()
+  return (
+    <ul className={clsx('flex flex-col', gap)}>
+      {sitesData.map((site) => {
+        const isActive = currentPath === `/${site.slug}`
+
+        return (
+          <li key={site.slug}>
+            <Link
+              href={`/${site.slug}`}
+              aria-current={isActive && 'page'}
+              className={clsx(
+                baseStyles,
+                sizeStyles['base'],
+                colourStyles.text['base'],
+                isActive ? colourStyles.active : colourStyles.inactive
+              )}
+            >
+              {site.displayName}
+              {isActive && <span className="sr-only"> (current page)</span>}
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
