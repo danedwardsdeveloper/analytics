d883d168c668c04f5977291f8f0de5982f7da557
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-10 11:36:04 +0000

    Simplify component

--- src/app/components/menuBar/index.tsx
+++ src/app/components/menuBar/index.tsx
@@ -1,29 +1,20 @@
 'use client'
-import DesktopMenu from './DesktopMenu'
-import { sitesData } from '@/library/sites'
-import MobileMenu from './MobileMenu'
 import { useApp } from '../AppProvider'
-
-export interface MenuItemInterface {
-  displayName: string
-  slug: string
-}
-
-const menuItems: MenuItemInterface[] = [{ displayName: 'Home', slug: '/' }]
+import MobileMenu from './mobileMenu'
+import DesktopMenu from './desktopMenu'
 
 export default function MenuBar() {
   const { signedIn } = useApp()
 
-  if (!signedIn) {
-    return null
-  }
+  if (!signedIn) return null
+
   return (
     <>
-      <div className="hidden md:block lg:w-72">
-        <DesktopMenu menuItems={menuItems} sites={sitesData} />
+      <div className="hidden md:block lg:w-72" aria-label="Sidebar">
+        <DesktopMenu />
       </div>
       <div className="block md:hidden">
-        <MobileMenu menuItems={menuItems} sites={sitesData} />
+        <MobileMenu />
       </div>
     </>
   )
