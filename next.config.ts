f2b3432e2b7009216ab4b703ce78b1ff51d043fa
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-10 11:32:59 +0000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone',
+  pageExtensions: ['ts', 'tsx'],
+  images: {
+    formats: ['image/webp'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
