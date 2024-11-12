788e4ced8a114f0afa9e4fe8afc24422c6637665
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-12 19:52:12 +0000

    Update dbConfig.tsx

---
+++ src/library/dbConfig.tsx
@@ -0,0 +1,11 @@
+export type DatabaseNames =
+  | 'dan-edwards-creative-analytics'
+  | 'dan-edwards-developer-analytics'
+  | 'array-of-sunshine-analytics'
+  | 'dan-digresses-analytics'
+
+export type TableNames =
+  | 'page-views'
+  | 'link-clicks'
+  | 'pre-saves'
+  | 'email-subscriptions'
