9d9da61d4cf2bc779be9dd9e78e49ead22fbcda1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-13 20:04:44 +0000

    Create Flag.tsx

---
+++ src/app/[slug]/siteAnalyticsLayout/countriesList/Flag.tsx
@@ -0,0 +1,12 @@
+import { countries, CountryKey } from './data'
+
+export default function Flag({ countryKey }: { countryKey: CountryKey }) {
+  const country = countries[countryKey]
+  const FlagComponent = country.component
+
+  return (
+    <div className="w-14 rounded-sm overflow-hidden" title={country.name}>
+      <FlagComponent />
+    </div>
+  )
+}
