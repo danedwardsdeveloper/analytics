d7d8b54e77d1ac95153b980c8ea0067f2a424c70
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-13 20:05:17 +0000

    Update dbConfig.ts

--- src/library/dbConfig.ts
+++ src/library/dbConfig.ts
@@ -9,3 +9,17 @@ export type TableNames =
   | 'link-clicks'
   | 'pre-saves'
   | 'email-subscriptions'
+
+interface DbConfig {
+  name: DatabaseNames
+  tables: TableNames[]
+}
+
+export const dbConfig: DbConfig = {
+  danEdwardsCreative: {
+    name: 'dan-edwards-creative-analytics',
+    tables: {
+      pageViews: 'page-views',
+    },
+  },
+}
