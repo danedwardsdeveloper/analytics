8e00b25e1f7b9bfef4cf8127e4c3069f4ed9fc6a
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-13 20:04:37 +0000

    Use presentational component

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -6,15 +6,13 @@ import { use, useEffect, useState } from 'react'
 import { PageViewsData } from '@/library/analyticsData'
 import { DatabaseNames } from '@/library/dbConfig'
 import { sitesData } from '@/library/sites'
+import { tempData } from '@/library/tempData'
 
 import { useApp } from '../../components/AppProvider'
-import Divider from '../../components/Divider'
-import PageHeader from '../../components/PageHeader'
-import DataCards from './components/DataCards'
-import GridList from './components/GridList'
-import HardcodedDataWarning from './components/HardcodedDataWarning'
 
-const USE_DATABASE = true
+import SiteAnalyticsLayout from './siteAnalyticsLayout'
+
+const USE_DATABASE = false
 
 interface Props {
   params: Promise<{
@@ -66,46 +64,19 @@ export default function SitePage({ params }: Props) {
     return null
   }
 
-  const formattedPageViewsData: PageViewsData = USE_DATABASE
-    ? {
-        title: 'Page views',
-        totalViews: pageViewsData.totalViews,
-        items: pageViewsData.items.map((page) => ({
-          name: page.name,
-          value: page.value,
-        })),
-      }
-    : {
-        title: 'Page views',
-        totalViews: 47,
-        items: [
-          { name: 'Home', value: 2456 },
-          { name: '/about', value: 1123 },
-          { name: '/contact', value: 864 },
-          { name: '/newsletter', value: 542 },
-        ],
-      }
-
   return (
-    <div className="space-y-8">
-      <PageHeader title={site.displayName} intro={site.description} />
-      <HardcodedDataWarning display={!USE_DATABASE} />
-      <Divider margin="my-6" />
-      <DataCards
-        titleOne="Page views"
-        valueOne={formattedPageViewsData.totalViews}
-        titleTwo="Sessions"
-        valueTwo={46}
-      />
-      <Divider margin="my-6" />
-      <div className="flex flex-col divide-y divide-gray-200">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 py-8">
-          <GridList
-            title={formattedPageViewsData.title}
-            items={formattedPageViewsData.items}
-          />
-        </div>
-      </div>
-    </div>
+    <SiteAnalyticsLayout
+      siteDisplayName={site.displayName}
+      siteDescription={site.description}
+      useHardcodedData={true}
+      totalViews={tempData.totalViews}
+      sessions={tempData.sessions}
+      emailSubscriptions={tempData.emailSubscriptions}
+      pageViewsData={tempData.pageViewsData}
+      countriesData={tempData.countriesData}
+      referralsData={tempData.referralsData}
+      devicesData={tempData.devicesData}
+      browsersData={tempData.browsersData}
+    />
   )
 }
