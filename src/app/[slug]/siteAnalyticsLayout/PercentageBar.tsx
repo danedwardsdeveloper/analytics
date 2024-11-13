177d3c94fc7956e5366b76c333298760077e373f
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-13 22:15:12 +0000

    Fix label handling

--- src/app/[slug]/siteAnalyticsLayout/PercentageBar.tsx
+++ src/app/[slug]/siteAnalyticsLayout/PercentageBar.tsx
@@ -19,7 +19,8 @@ export default function PercentageBar({
         <div className="absolute inset-0 flex items-center px-2 h-8">
           <div className="relative z-10 flex items-center">
             <span className="whitespace-nowrap">
-              {`${label} ${percentage}`}%
+              {label && <>{label} </>}
+              {percentage}%
             </span>
             <span
               className={clsx(
