3b8d47362d45d54db26f248daa58e3e57b34614b
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-13 20:04:52 +0000

    Create DataList.tsx

---
+++ src/app/[slug]/siteAnalyticsLayout/DataList.tsx
@@ -0,0 +1,70 @@
+import { ReactNode } from 'react'
+
+import PercentageBar from './PercentageBar'
+
+interface Item {
+  name: string
+  value: number
+}
+
+export function DataListContainer({
+  title,
+  children,
+  items,
+  unit,
+  percentage,
+}: {
+  title: string
+  children?: ReactNode
+  items?: Item[]
+  unit?: string
+  percentage?: boolean
+}) {
+  return (
+    <div className="flex flex-col gap-y-4 w-full">
+      <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
+      <>
+        {children}
+        {items && (
+          <DataListItems items={items} unit={unit} percentage={percentage} />
+        )}
+      </>
+    </div>
+  )
+}
+
+interface DataListItemsProps {
+  items: Item[]
+  unit?: string
+  percentage?: boolean
+}
+
+export function DataListItems({ items, unit, percentage }: DataListItemsProps) {
+  return (
+    <>
+      {items.map((item) =>
+        percentage ? (
+          <>
+            <div
+              key={item.name}
+              className="flex justify-between items-center h-8"
+            >
+              <PercentageBar label={item.name} percentage={item.value} />
+            </div>
+          </>
+        ) : (
+          <div
+            key={item.name}
+            className="flex justify-between items-center h-8"
+          >
+            <span className="text-gray-600">{item.name}</span>
+            <span className="text-gray-900">
+              {item.value.toLocaleString()}
+              {unit && <span className="ml-0.5 text-gray-600">{unit}</span>}
+            </span>
+          </div>
+        ),
+      )}
+    </>
+  )
+}
