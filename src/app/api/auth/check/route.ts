abd275dfcc6f89760187f3720bc2b10b27f5bcd8
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-15 10:09:27 +0000

    Remove console logs

--- src/app/api/auth/check/route.ts
+++ src/app/api/auth/check/route.ts
@@ -6,8 +6,6 @@ import { cookieName } from '@/library/cookies'
 export async function GET() {
   const cookieStore = await cookies()
   const token = cookieStore.get(cookieName)
-  console.log('Token cookie:', token)
-
   const isAuthenticated = token !== undefined && token !== null
   return NextResponse.json({ authenticated: isAuthenticated })
 }
