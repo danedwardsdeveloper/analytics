e7d490332ce6a53d01868f989e9a855b990eb9f1
Dan Edwards <daniel.edwards96@yahoo.com>
2024-11-15 10:09:15 +0000

    Update DataList.tsx

--- src/app/[slug]/siteAnalyticsLayout/DataList.tsx
+++ src/app/[slug]/siteAnalyticsLayout/DataList.tsx
@@ -10,65 +10,64 @@ interface Item {
 export function DataListContainer({
   title,
   children,
-  items,
-  unit,
-  percentage,
 }: {
   title: string
   children?: ReactNode
-  items?: Item[]
-  unit?: string
-  percentage?: boolean
 }) {
   return (
     <div className="flex flex-col gap-y-4 w-full">
       <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
-      <>
-        {children}
-        {items && (
-          <DataListItems items={items} unit={unit} percentage={percentage} />
-        )}
-      </>
+      {children}
     </div>
+  )
+}
+
+interface PercentageDataItemsProps {
+  items: Item[]
+  unit?: string
+  percentage?: boolean
+}
+
+export function PercentageDataItems({ items }: PercentageDataItemsProps) {
+  return (
+    <>
+      {items.map((item, index) => (
+        <div
+          key={`${item.name}${item.value}${index}`}
+          className="flex justify-start items-center gap-2 group h-8"
+        >
+          <div className="w-14 h-full bg-gray-300 rounded-sm overflow-hidden" />
+          <PercentageBar
+            key={`${index}${item.name}${item.value}`}
+            percentage={item.value}
+            hoverText={item.name}
+          />
+        </div>
+      ))}
+    </>
   )
 }
 
 interface DataListItemsProps {
   items: Item[]
   unit?: string
-  percentage?: boolean
 }
 
-export function DataListItems({ items, unit, percentage }: DataListItemsProps) {
+export function DataListItems({ items, unit }: DataListItemsProps) {
   return (
     <>
-      {items.map((item, index) =>
-        percentage ? (
-          <>
-            <div
-              key={`${item.name}${item.value}${index}`}
-              className="flex justify-between items-center h-8"
-            >
-              <PercentageBar
-                key={`${index}${item.name}${item.value}`}
-                label={item.name}
-                percentage={item.value}
-              />
-            </div>
-          </>
-        ) : (
-          <div
-            key={`${item.name}${item.value}${index}`}
-            className="flex justify-between items-center h-8"
-          >
-            <span className="text-gray-600">{item.name}</span>
-            <span className="text-gray-900">
-              {item.value.toLocaleString()}
-              {unit && <span className="ml-0.5 text-gray-600">{unit}</span>}
-            </span>
-          </div>
-        ),
-      )}
+      {items.map((item, index) => (
+        <div
+          key={`${item.name}${item.value}${index}`}
+          className="flex justify-between items-center h-8"
+        >
+          <span className="text-gray-600">{item.name}</span>
+          <span className="text-gray-900">
+            {item.value.toLocaleString()}
+            {unit && <span className="ml-0.5 text-gray-600">{unit}</span>}
+          </span>
+        </div>
+      ))}
     </>
   )
 }
